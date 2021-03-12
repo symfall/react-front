3d35b6f12d0046c7aaad2d14b07354ac1e10a2dd
Vlad <vladmik19@gmail.com>
2021-03-12 14:50:00 +0300

    Create base Header style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import logo from '../assets/images/logo.png'
 
 export default function Header () {
+
+    const links = [
+        {href: '/', title: 'Home'},
+        {href: '#', title: 'Chat'},
+        {href: '#', title: 'Profile'},
+        {href: '#', title: 'Settings'},
+    ]
+
     return (
-        <p>Header</p>
+        <header className='appHeader'>
+            <Link to='/' className='blockLogo'>
+                <img src={logo} alt="logo" className='headerLogo'/>
+            </Link>
+            <nav className='headerNav'>
+                <ul className='navList'>
+                    {links.map(index => 
+                        <li className='navItem'>
+                            <Link className='navLink' to={index.href}>
+                                {index.title}
+                            </Link>
+                        </li>
+                    )}
+                </ul>
+            </nav>
+        </header>
     )
 }
